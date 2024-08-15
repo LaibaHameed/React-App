6e30ae96befbf5f3bb21675d57ba47bf5443c8f8
Laiba_012 <laibahameed012@gmail.com>
2024-08-15 11:41:24 +0500

    added

--- demo-project/src/pages/Home/Home.jsx
+++ demo-project/src/pages/Home/Home.jsx
@@ -18,22 +18,33 @@ const Home = () => {
                 <div className="row">
 
                     <div className="content">
-                        <h3 className="freshCoffee">fresh coffee in Morning</h3>
-                        <a routerLink="/menu" className="btn"> Buy one Now</a>
+                        <h3 className="freshCoffee">
+                        {/* Every Sip is a Step to Bliss */}
+                        Fresh Coffee for You
+                        {/* Your Perfect Coffee, Brewed with Care */}
+                        {/* Sip,Savor, Energize Your Day */}
+                        </h3>
+                        <p>Your Coffee Deserves the Best, and We're Here to Pour It for You.
+                        Your Morning Deserves the Best, and We're Here to Brew It for You.</p>
+                        <div className='btn-btn'>
+                        <a routerLink="/menu" className="btn"> book Table</a>
+                        <a routerLink="/menu" className=" btn-line"> Buy Now</a>
+                        </div>
                     </div>
 
                     <div className="image">
                         {/* <img src="../../assets/imgs/home-img-1.png"  alt="" /> */}
-                        <img className="main-img" src={coffeeImage} alt="Coffee" />
+                        <img className="main-img" src={coffeeImage3} alt="Coffee" />
+                        
                     </div>
 
                 </div>
 
-                <div className="img-slider">
+                {/* <div className="img-slider">
                     <img   src={coffeeImage2} alt="" />
                     <img src={coffeeImage3} alt="" />
                     <img className='img-slide1' src={coffeeImage1} alt="" />
-                </div>
+                </div> */}
 
             </div>
             <Section1/>
